1418874adbcfe031a1a1f014a43d57651f194bcc
thomy-fauzi <thomy.learning@gmail.com>
2025-04-09 05:33:07 +0700

    Feat: Update Personality and Interests

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const cors = require("cors");
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
 const UserControllers = require("./controllers/userController");
+const Controller = require("./controllers/controller");
+const authentication = require("./middlewares/authentication");
 const app = express();
 const port = 3000;
 app.use(cors());
@@ -16,6 +18,9 @@ app.get("/", (req, res) => {
 
 app.post("/register", UserControllers.register);
 app.post("/login", UserControllers.login);
+
+app.use(authentication);
+app.put("/update", Controller.addPersonalityAndInterest);
 
 app.use(errorHandler);
 app.listen(port, () => {
