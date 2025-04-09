d7e2d77d3b0fa9c05589f94b980021496ac50fc2
veryal <ferdiahmdi@gmail.com>
2025-04-09 13:48:26 +0700

    feat: GenAI buat match, add .env.example file, update package.json buat Google GenAI

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,4 +1,6 @@
+const { Op } = require("sequelize");
 const { User, Room } = require("../models");
+const { GoogleGenAI } = require("@google/genai");
 
 class Controller {
   static async addPersonalityAndInterest(req, res, next) {
@@ -31,8 +33,8 @@ class Controller {
           name: user.name,
           email: user.email,
           interests: user.interests,
-          personalities: user.personalities,
-        },
+          personalities: user.personalities
+        }
       });
     } catch (err) {
       next(err);
@@ -46,7 +48,7 @@ class Controller {
       if (users.length < 2) {
         throw {
           name: "NotFound",
-          message: "Not enough users to create a room",
+          message: "Not enough users to create a room"
         };
       }
 
@@ -57,8 +59,8 @@ class Controller {
       const existingRoom = await Room.findOne({
         where: {
           user1Id: user1.id,
-          user2Id: user2.id,
-        },
+          user2Id: user2.id
+        }
       });
 
       if (existingRoom) {
@@ -68,7 +70,7 @@ class Controller {
       // Create a new room
       const newRoom = await Room.create({
         user1Id: user1.id,
-        user2Id: user2.id,
+        user2Id: user2.id
       });
 
       res.status(201).json({
@@ -77,14 +79,67 @@ class Controller {
           id: newRoom.id,
           user1: {
             id: user1.id,
-            name: user1.name,
+            name: user1.name
           },
           user2: {
             id: user2.id,
-            name: user2.name,
-          },
-        },
+            name: user2.name
+          }
+        }
       });
+    } catch (err) {
+      next(err);
+    }
+  }
+
+  // Get matching partner from Google GenAI based on interests and personalities
+  static async getMatchingPartner(req, res, next) {
+    try {
+      const userId = req.user.id;
+      const user = await User.findByPk(userId);
+
+      // Sementara pake ini kalo mau ganti match
+      const { lastMatch } = req.query;
+      const excludedId = lastMatch ? [lastMatch, userId] : [userId];
+
+      if (!user) {
+        throw { name: "NotFound", message: "User not found" };
+      }
+
+      const { interests, personalities } = user;
+      console.log(interests, personalities);
+
+      const fetchUsersInterests = await User.findAll({
+        attributes: ["id", "name", "interests", "personalities"],
+        where: {
+          id: {
+            [Op.notIn]: excludedId
+          }
+        }
+      });
+
+      // console.log(JSON.stringify(fetchUsersInterests, null, 2));
+
+      const ai = new GoogleGenAI({ apiKey: process.env.GENAI_API_KEY });
+
+      const response = await ai.models.generateContent({
+        model: "gemini-2.0-flash",
+        contents: `
+          Find me one person from this data: 
+          ${JSON.stringify(fetchUsersInterests, null, 2)} 
+          that best matches me in romace according to my interests: ${interests} and personalities: ${personalities}. Then output the result in JSON format with the following attributes: id, name, interests, personalities of the matching person.
+
+          Lastly, add a "message" key to the JSON object with the value of a single sentence describing the match between me and the person in romance.
+
+          Only output the JSON object, nothing else.
+          `
+      });
+      const matchingPartner = JSON.parse(
+        response.text.replace("```json", "").replace("```", "")
+      );
+      console.log(matchingPartner);
+
+      res.status(200).json(matchingPartner);
     } catch (err) {
       next(err);
     }
